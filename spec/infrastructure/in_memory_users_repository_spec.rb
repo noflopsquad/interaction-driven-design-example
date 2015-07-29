3eab74c70cfba271628f5bf08cbd027e9466077d
elreplicante <replymusic@gmail.com>
2015-07-29 11:27:20 +0200

    using shared examples for infrastucture

---
+++ spec/infrastructure/in_memory_users_repository_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'infrastructure/repository_registry'
+require 'infrastructure/users/repositories/in_memory'
+require_relative 'users_repository_spec'
+
+describe "In memory users repository" do   
+
+  before do
+    RepositoryRegistry.register(:user, Users::Repositories::InMemory.new)
+    @repo = RepositoryRegistry.for(:user)
+  end
+
+  it_behaves_like "users repository"
+end
