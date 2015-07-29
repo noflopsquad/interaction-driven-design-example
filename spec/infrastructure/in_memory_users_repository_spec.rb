9af3d4f4f0d167303eed72bae0b351789103029f
trikitrok <trikitrok@gmail.com>
2015-07-29 13:56:09 +0200

    Renamed file to avoid users repository shared examples being loaded twice

--- spec/infrastructure/in_memory_users_repository_spec.rb
+++ spec/infrastructure/in_memory_users_repository_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 require 'infrastructure/repository_registry'
 require 'infrastructure/users/repositories/in_memory'
-require_relative 'users_repository_spec'
+require_relative 'users_repository_contract'
 
-describe "In memory users repository" do   
+describe "In memory users repository" do
 
   before do
     RepositoryRegistry.register(:user, Users::Repositories::InMemory.new)
