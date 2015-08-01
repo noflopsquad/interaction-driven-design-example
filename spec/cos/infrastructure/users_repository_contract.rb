7be17d8153ea043bbedf574af8a5271d6c25a655
trikitrok <trikitrok@gmail.com>
2015-08-01 11:58:36 +0200

    Improved documentation otuput of repository contract spec

--- spec/cos/infrastructure/users_repository_contract.rb
+++ spec/cos/infrastructure/users_repository_contract.rb
@@ -2,34 +2,40 @@ require 'spec_helper'
 
 RSpec.shared_examples "users repository" do
 
-  describe "Registering a user" do
-    let(:user_name) { '@foolano' }
+  let(:user_name) { '@foolano' }
+  let(:follower_name) { '@zutano' }
+  let(:followed_name) { '@mengano' }
 
-    it "registers a new user if username is not registered" do
-      @repo.register(user_name)
+  it "knows when a user is registered" do
+    given_already_registered(user_name)
 
-      expect(@repo.registered?(user_name)).to be_truthy
-    end
-
-    it "knows when a user is not registered" do
-      expect(@repo.registered?(user_name)).to be_falsy
-    end
+    expect(@repo.registered?(user_name)).to be_truthy
   end
 
-  describe "Adding a follower to a user" do
-    let(:follower_name) { '@foolano' }
-    let(:followed_name) { '@mengano' }
-
-    describe "when both users are registered" do
-      it "adds a follower to a followed user" do
-        @repo.register(follower_name)
-        @repo.register(followed_name)
-
-        @repo.add_follower(follower_name, followed_name)
-
-        expect(@repo.followers_of(followed_name)).to eq [follower_name]
-      end
-    end
+  it "knows when a user is not registered" do
+    expect(@repo.registered?(user_name)).to be_falsy
   end
 
+  it "registers a new user if username is not registered" do
+    @repo.register(user_name)
+
+    expect(@repo.registered?(user_name)).to be_truthy
+  end
+
+  it "adds a follower to a user" do
+    given_both_already_registered(follower_name, followed_name)
+
+    @repo.add_follower(follower_name, followed_name)
+
+    expect(@repo.followers_of(followed_name)).to eq [follower_name]
+  end
+
+  def given_both_already_registered(follower_name, followed_name)
+    given_already_registered(follower_name)
+    given_already_registered(followed_name)
+  end
+
+  def given_already_registered(user_name)
+    @repo.register(user_name)
+  end
 end
